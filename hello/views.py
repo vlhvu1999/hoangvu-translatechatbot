53ba786ed1e05fabbd88eb13ca5bfcbddf149a8b
vlhvu <70124551+vlhvu@users.noreply.github.com>
2020-12-09 23:56:48 +0700

    ok

---
+++ hello/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Greeting
+
+from googletrans import Translator
+from django.views.decorators.csrf import csrf_exempt
+from langdetect import detect
+import json
+import pychatwork as ch
+import re
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Python!')
+    payload = str(request.body, encoding='utf-8')
+    return render(request, "index.html")
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
+# Create your views here.
+def decode_payload(request):
+    payload = str(request.body, encoding='utf-8')
+    return json.loads(payload)
+
+@csrf_exempt
+def chatwork_webhook(request):
+    #ACCOUNT_ID_BOT = 5130876
+    CHECK = "#"
+
+
+    payload = decode_payload(request)
+    messageChat = payload["webhook_event"]["body"]
+
+    #systax translate
+
+    if not CHECK in messageChat:
+        return HttpResponse('Webhook received', status=200)
+    elif CHECK != messageChat[0]:
+        return HttpResponse('Webhook received', status=200)
+    messageChat = messageChat.replace(CHECK,"")
+
+
+
+    """#account_id bot not translate
+    accountId = payload["webhook_event"]["account_id"]
+    if accountId == ACCOUNT_ID_BOT:
+        return HttpResponse('Webhook received', status=200)"""
+
+    #translate message
+    translator = Translator()
+    lang = detect(messageChat)
+
+    locale = "vi"
+    if lang == "vi":
+        locale = "ja"
+    translated = translator.translate(messageChat, src=lang, dest=locale).text
+
+    #Send Data back to chatwork
+    client = ch.ChatworkClient('48583a895ab285d6e0f32ee5ce2c8089')
+    # get message from room 1234
+    res = client.get_messages(room_id='197197227', force=True)
+    # post message to room 1234
+    client.post_messages(room_id='197197227', message=translated)
+
+    return HttpResponse('Webhook received', status=200)
