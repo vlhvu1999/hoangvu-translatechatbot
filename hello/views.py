c096304b820f7f7ed938b21927880adf019bc80b
vlhvu <70124551+vlhvu@users.noreply.github.com>
2020-12-10 09:35:02 +0700

    test fix change

--- hello/views.py
+++ hello/views.py
@@ -29,42 +29,4 @@ def decode_payload(request):
 
 @csrf_exempt
 def chatwork_webhook(request):
-    #ACCOUNT_ID_BOT = 5130876
-    CHECK = "#"
-
-
-    payload = decode_payload(request)
-    messageChat = payload["webhook_event"]["body"]
-
-    #systax translate
-
-    if not CHECK in messageChat:
-        return HttpResponse('Webhook received', status=200)
-    elif CHECK != messageChat[0]:
-        return HttpResponse('Webhook received', status=200)
-    messageChat = messageChat.replace(CHECK,"")
-
-
-
-    """#account_id bot not translate
-    accountId = payload["webhook_event"]["account_id"]
-    if accountId == ACCOUNT_ID_BOT:
-        return HttpResponse('Webhook received', status=200)"""
-
-    #translate message
-    translator = Translator()
-    lang = detect(messageChat)
-
-    locale = "vi"
-    if lang == "vi":
-        locale = "ja"
-    translated = translator.translate(messageChat, src=lang, dest=locale).text
-
-    #Send Data back to chatwork
-    client = ch.ChatworkClient('fd0602c43dd83cae39e7ebfb08d5793d')
-    # get message from room 1234
-    res = client.get_messages(room_id='197925987', force=True)
-    # post message to room 1234
-    client.post_messages(room_id='197925987', message=translated)
-
     return HttpResponse('Webhook received', status=200)
