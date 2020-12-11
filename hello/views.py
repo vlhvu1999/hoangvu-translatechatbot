7c08ff3f5cc67d4363bd7aaf2f612fb42b551a64
vlhvu <70124551+vlhvu@users.noreply.github.com>
2020-12-11 14:13:57 +0700

    change 6

--- hello/views.py
+++ hello/views.py
@@ -63,7 +63,9 @@ def chatwork_webhook(request):
     #translated = translator.translate(messageChat, src=lang, dest=locale).text
 
     translator = Translator()
-    t = translator.translate(messageChat, src='vi', dest='ja')
+    translated = translator.translate(messageChat, src='vi', dest='ja')
+
+
 
 
 
@@ -74,7 +76,7 @@ def chatwork_webhook(request):
     res = client.get_messages(room_id='197925987', force=True)
 
     # post message to room 1234
-    client.post_messages(room_id='197925987', message=t.text)
+    client.post_messages(room_id='197925987', message=translated.text)
 
 
     return HttpResponse('Webhook received', status=200)
