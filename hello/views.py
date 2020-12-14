d86c8fa50d36a3b9a59888eb9ff026e26543c7b3
vlhvu <70124551+vlhvu@users.noreply.github.com>
2020-12-14 11:43:00 +0700

    Kkk

--- hello/views.py
+++ hello/views.py
@@ -1,76 +1,63 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Greeting
-
-from googletrans import Translator
-
-from django.views.decorators.csrf import csrf_exempt
-from langdetect import detect
-import json
 import pychatwork as ch
-import re
+from django.views.decorators.csrf import csrf_exempt
+import json
+from googletrans import Translator
+from langdetect import detect
+from pydrive.auth import GoogleAuth
+
+
+# from .models import Greeting
 
 # Create your views here.
+
+
 def index(request):
     # return HttpResponse('Hello from Python!')
     payload = str(request.body, encoding='utf-8')
     return render(request, "index.html")
-def db(request):
 
-    greeting = Greeting()
-    greeting.save()
 
-    greetings = Greeting.objects.all()
+# def db(request):
 
-    return render(request, "db.html", {"greetings": greetings})
+#     greeting = Greeting()
+#     greeting.save()
+
+#     greetings = Greeting.objects.all()
+
+#     return render(request, "db.html", {"greetings": greetings})
 # Create your views here.
+
+
 def decode_payload(request):
     payload = str(request.body, encoding='utf-8')
     return json.loads(payload)
 
+
 @csrf_exempt
 def chatwork_webhook(request):
 
-    ACCOUNT_ID_BOT = 5130876
-    CHECK = "#"
-
     payload = decode_payload(request)
+
     messageChat = payload["webhook_event"]["body"]
-
-    #systax translate
-    if not CHECK in messageChat:
-        return HttpResponse('Webhook received', status=200)
-    elif CHECK != messageChat[0]:
-        return HttpResponse('Webhook received', status=200)
-    messageChat = messageChat.replace(CHECK,"")
+    messageChat1 = messageChat.replace("[To:5130876]Bot_Translate", "")
 
 
-    #account_id bot not translate
-    accountId = payload["webhook_event"]["account_id"]
-    if accountId == ACCOUNT_ID_BOT:
-        return HttpResponse('Webhook received', status=200)
+translator = Translator()
+lang = detect(messageChat1)
+locale = "vi"
+if lang == "vi":
+    locale = "ja"
+translated = translator.translate(messageChat1, src=lang, dest=locale)
 
 
-    #translate message
-    translator = Translator()
+# Send Data back to chatwork
+client = ch.ChatworkClient('fd0602c43dd83cae39e7ebfb08d5793d')
+# get message from room 1234
+res = client.get_messages(room_id='197925987', force=True)
 
-    lang = detect(messageChat)
+# post message to room 1234
+client.post_messages(room_id='197925987', message=translated.text)
 
-    locale = "vi"
-    if lang == "vi":
-        locale = "ja"
-
-    translated = translator.translate(messageChat, src=lang, dest=locale)
-
-
-    #Send Data back to chatwork
-    client = ch.ChatworkClient('fd0602c43dd83cae39e7ebfb08d5793d')
-
-    # get message from room 1234
-    res = client.get_messages(room_id='197925987', force=True)
-
-    # post message to room 1234
-    client.post_messages(room_id='197925987', message=translated.text)
-
-
-    return HttpResponse('Webhook received', status=200)
+# return HttpResponse('Webhook received', status=200)
