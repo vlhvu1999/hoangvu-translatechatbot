51076d1331f8b4e682b63dc534d6f6242a57148e
vlhvu1999 <vlhvu1612.ivs@gmail.com>
2020-12-16 09:21:31 +0700

    change account id

--- hello/views.py
+++ hello/views.py
@@ -67,10 +67,10 @@ def chatwork_webhook(request):
     client = ch.ChatworkClient('fd0602c43dd83cae39e7ebfb08d5793d')
 
     # get message from room 1234
-    res = client.get_messages(room_id='197925987', force=True)
+    res = client.get_messages(account_id=accountId, force=True)
 
     # post message to room 1234
-    client.post_messages(room_id='197925987', message=translated)
+    client.post_messages(account_id=accountId, message=translated)
 
 
     return HttpResponse('Webhook received', status=200)
